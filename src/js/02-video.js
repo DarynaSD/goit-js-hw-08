419dc6e0b2a4707c76e957f03f78a87caf2192b2
DarynaSD <dasha.dermanchuk@gmail.com>
2023-07-13 18:51:39 +0300

    task 2 - hw 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+// додати як залежність (через npm) бібліотеки Vimeo і Lodash
 
+//Ініціалізація плеєра у файлі скрипта 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const locStorKey = 'videoplayer-current-time';
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+
+player.on('timeupdate', throttle(play, 1000));
+
+function play(event) {
+  localStorage.setItem(locStorKey, event.seconds);
+};
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(locStorKey)) || 0);
